4fd73f8aa5818c3f39f22454a4988e85e085f5b6
A-n-d-r-e-s-i-l-l-o <lopeztavo07.20@gmail.com>
2026-05-09 13:24:53 -0500

    Creacion de ejercicios y nueva carpeta

---
+++ Fundamentos/FormateadorNombre/Program.cs
@@ -0,0 +1,11 @@
+﻿class FormateadorNombres
+{
+    static void Main()
+    {
+        Console.Write("Ingrese su nombre: ");
+        string nombre = Console.ReadLine().ToUpper();
+        Console.Write("Ingrese su apellido: ");
+        string apellido = Console.ReadLine().ToUpper();
+        Console.WriteLine($"Bienvenido {nombre.Trim()} {apellido.Trim()}");
+    }
+}
