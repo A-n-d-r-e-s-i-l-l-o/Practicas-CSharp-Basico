f7ce172a89dd82eb73eeca973941001058d3a2cb
A-n-d-r-e-s-i-l-l-o <lopeztavo07.20@gmail.com>
2026-06-17 21:25:25 -0500

    1 problema añadido

--- Funciones/NotasAlumnos/Program.cs
+++ Funciones/NotasAlumnos/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System;
+using System.Timers;
+void GenerarReporteEstudiante(string? estudiante,double nota1,double nota2)
+{
+    string estado = ((nota1+nota2)/2)>=11? "Aprobado" : "Desaprobado";
+    Console.WriteLine($@"Estudiante: {estudiante}
+La notas son {nota1} y {nota2}
+El promedio es {(nota1+nota2)/2}
+El alumno esta {estado}");    
+}
+
+Console.Write("Ingrese el nombre del estudiante: ");
+string? nombre = Console.ReadLine();
+Console.Write("Ingrese la nota 1 del estudiante: ");
+double nota1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Ingrese la nota 2 del estudiante: ");
+double nota2 = Convert.ToDouble(Console.ReadLine());
+GenerarReporteEstudiante(nombre,nota1,nota2);
